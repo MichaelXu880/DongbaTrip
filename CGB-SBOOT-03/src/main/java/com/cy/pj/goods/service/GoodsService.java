511f2f5ae1cf879433f439b076de914a0bbffee7
Jason <qlfreesky@gmail.com>
2020-06-05 09:47:41 +0800

    add mapper.xml

--- CGB-SBOOT-03/src/main/java/com/cy/pj/goods/service/GoodsService.java
+++ CGB-SBOOT-03/src/main/java/com/cy/pj/goods/service/GoodsService.java
@@ -6,10 +6,10 @@ import com.cy.pj.goods.pojo.Goods;
  * 商品模块业务层接口，负责商品业务的定义及处理
  */
 public interface GoodsService {
+	
+	 
    
 	 int saveObject(Goods entity);
-	
 	 List<Goods> findObjects();
-	 
 	 int deleteById(Integer id);
 }
