d78d73b302e89661fb0cae07db7a17b827d5c2f8
Jason <qlfreesky@gmail.com>
2020-06-06 15:02:20 +0800

    use lombok

--- CGB-Activity-01/src/main/java/com/cy/pj/activity/pojo/Activity.java
+++ CGB-Activity-01/src/main/java/com/cy/pj/activity/pojo/Activity.java
@@ -6,10 +6,20 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**活动模块用于存储活动信息的一个对象*/
-@Data
+//@Data
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor  //无参构造
+@AllArgsConstructor //所有参数的构造函数
 public class Activity {
 
 	private Long id;
@@ -26,5 +36,9 @@ public class Activity {
 	private String remark;
 	private Date createdTime;
 	private String createdUser;
+	
+	public Activity(Long id) {
+		this.id=id;
+	}
 	
 }
--- CGB-Activity-01/src/test/java/com/cy/lombok/LombokTests.java
+++ CGB-Activity-01/src/test/java/com/cy/lombok/LombokTests.java
@@ -1,14 +1,20 @@
 package com.cy.lombok;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.cy.pj.activity.pojo.Activity;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 public class LombokTests {
-
+	  //private static final Logger log=
+	  //LoggerFactory.getLogger(LombokTests.class);
 	  @Test
 	  public void testActivity() {
 		  Activity aty=new Activity();
@@ -16,8 +22,12 @@ public class LombokTests {
 		  String title=aty.getTitle();
 		  System.out.println(title);
 		  System.out.println(aty);
+		  log.info("aty.title is {}",title);
 	  }
 }
+
+
+
 
 
 
