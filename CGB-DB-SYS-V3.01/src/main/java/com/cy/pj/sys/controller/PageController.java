31d035ee8c91cab7fafc465ea392b832ca980eaf
Jason <qlfreesky@gmail.com>
2020-06-10 12:03:17 +0800

    do create project

---
+++ CGB-DB-SYS-V3.01/src/main/java/com/cy/pj/sys/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.cy.pj.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 计划：所有涉及到页面返回的方法都定义在此Controller中
+ * @author pc
+ */
+@Controller
+@RequestMapping("/")
+public class PageController {
+	  @RequestMapping("doIndexUI")
+	  public String doIndexUI() {
+		  return "starter";
+	  }
+}
