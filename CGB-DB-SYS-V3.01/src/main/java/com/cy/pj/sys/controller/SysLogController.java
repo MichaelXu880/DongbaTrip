afeb380e2c8635a4214f1fbafaffe921b2a6749b
Jason <qlfreesky@gmail.com>
2020-06-12 16:20:45 +0800

    do handle exception

--- CGB-DB-SYS-V3.01/src/main/java/com/cy/pj/sys/controller/SysLogController.java
+++ CGB-DB-SYS-V3.01/src/main/java/com/cy/pj/sys/controller/SysLogController.java
@@ -2,6 +2,7 @@ package com.cy.pj.sys.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,9 +24,8 @@ public class SysLogController {
 			Integer pageCurrent){
 		PageObject<SysLog> pageObject=
 		sysLogService.findPageObjects(username, pageCurrent);
-		return new JsonResult(pageObject);
+		return new JsonResult(pageObject);//DispatcherServlet
 	}
-	
 	
 }
 
