369f7d8f5bf417d1006cb97786ed22e466cf819b
'Jason' <'Jason@gmail.com'>
2020-06-16 14:05:20 +0800

    do ztree demo

---
+++ CGB-ZTREE-01/src/main/java/com/cy/CgbZtree01Application.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgbZtree01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgbZtree01Application.class, args);
+	}
+
+}
---
+++ CGB-ZTREE-01/src/test/java/com/cy/CgbZtree01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgbZtree01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
