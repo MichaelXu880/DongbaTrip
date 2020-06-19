8d0d1dbf6833959b28d673cbc7bb8e38b3b7031b
'Jason' <'Jason@gmail.com'>
2020-06-19 23:21:06 +0800

    do update

--- CGB-DB-SYS-V3.01/src/main/java/com/cy/Application.java
+++ CGB-DB-SYS-V3.01/src/main/java/com/cy/Application.java
@@ -2,7 +2,7 @@ package com.cy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//启动类
+
 @SpringBootApplication
 public class Application {
 
